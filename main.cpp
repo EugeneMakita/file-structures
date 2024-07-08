082f0041708b5c6625c49cd502dded2f9e3cca92
Eugene Gatawa Makita <73113215+EugeneMakita@users.noreply.github.com>
2024-07-07 23:18:54 -0700

    add operator overloadings

--- main.cpp
+++ main.cpp
@@ -3,17 +3,29 @@
 #include<ctime>
 #include "courseRegistration.h"
 
+std::istream& operator>>(std::istream &input, Student &student){
+    input >> student.id >> student.name >> student.surname >> student.credit_hours;
+    return input;
+}
 
+std::ostream& operator<<(std::ostream &output, Student student){
+    output << "ID: " << student.id << " ,name: " <<student.name << " ,surname: " <<student.surname << " ,credit hours: " << student.credit_hours << " " << std::endl;
+    return output;
+}
 int main(){
     Student student("034FC", "john", "mangudya", 23);
-    std::cout << "ID: " << student.id << " ,name: " <<student.name << " ,surname: " <<student.surname << " ,credit hours: " << student.credit_hours << " " << std::endl;
+    std::cout << student;
 
     Student student1;
     student1 = student;
-    std::cout << "ID: " << student1.id << " ,name: " <<student1.name << " ,surname: " <<student1.surname << " ,credit hours: " << student1.credit_hours << " " << std::endl;
+    std::cout << student1;
 
     CourseRegistration course("History", student1, 40, 'A');
     std::cout << "Course Name: " << course.name << " ,student: " << course.student.get_full_name() << " ,credit hours: " <<course.credit_hours << " , grade: " << course.grade << " " << std::endl;
+
+    Student student2;
+    std::cin >> student2; 
+    std::cout << student2;
 
     return 0;
 }
--- student.h
+++ student.h
@@ -10,5 +10,6 @@ class Student{
         Student(std::string id, std::string name, std::string surname, int credit_Hours);
         void operator=(Student student);
         std::string get_full_name();
+        /*std::istream& operator>>(std::istream &input);*/
 };
 #endif
