b01ab2b523b66daa7c2cc1f80e05397220185a65
Eugene Gatawa Makita <73113215+EugeneMakita@users.noreply.github.com>
2024-07-11 15:35:20 -0700

    separate headers with text

--- FixedTextBuffer.cpp
+++ FixedTextBuffer.cpp
@@ -1,107 +1,93 @@
 #include<iostream>
 #include<fstream>
 #include "personFixed.h"
+#include "FixedTextBuffer.h"
 
-class FixedTextBuffer{
-    char *Buffer;
-    int BufferSize;
-    int MaxBytes;
-    int NextBytes;
-    int *FieldSizes;
-    int MaxFields;
-    int NumberOfFields;
-    int Packing;
-    int NextField;
-    int NextCharecter;
+FixedTextBuffer:: FixedTextBuffer(int MaxBytes, int MaxFields){
+    this->MaxBytes = MaxBytes;
+    this->Buffer = new char[this->MaxBytes];
+    this->MaxFields = MaxFields;
+    this->FieldSizes = new int[MaxFields];
+    this->BufferSize = 0;
+    this->NextBytes = 0;
+    this->NumberOfFields = 0;
+    this->NextField = 0;
+    this->NextCharecter = 0;
+    this->Packing = true;
+}
 
-    public:
-        FixedTextBuffer(int MaxBytes=60, int MaxFields=6){
-            this->MaxBytes = MaxBytes;
-            this->Buffer = new char[this->MaxBytes];
-            this->MaxFields = MaxFields;
-            this->FieldSizes = new int[MaxFields];
-            this->BufferSize = 0;
-            this->NextBytes = 0;
-            this->NumberOfFields = 0;
-            this->NextField = 0;
-            this->NextCharecter = 0;
-            this->Packing = true;
-        }
+int FixedTextBuffer::AddField(int FieldSize){
+    if (NumberOfFields >= MaxFields){
+        return false;
+    }
 
-        int AddField(int FieldSize){
-            if (NumberOfFields >= MaxFields){
-                return false;
-            }
+    if ((BufferSize+FieldSize) > MaxBytes){
+        return false;
+    }
 
-            if ((BufferSize+FieldSize) > MaxBytes){
-                return false;
-            }
+    FieldSizes[NumberOfFields] = FieldSize;
+    NumberOfFields++;
+    BufferSize += FieldSize;
+    return true;
+}
 
-            FieldSizes[NumberOfFields] = FieldSize;
-            NumberOfFields++;
-            BufferSize += FieldSize;
-            return true;
-        }
+int FixedTextBuffer::GetNumberOfFields() const{
+    return NumberOfFields;
+}
 
-        int GetNumberOfFields() const{
-            return NumberOfFields;
-        }
+void FixedTextBuffer::Clear(){
+    this->Buffer = new char[this->MaxBytes];
+    this->NextBytes = 0;
+    this->NextField = 0;
+}
 
-        void Clear(){
-            this->Buffer = new char[this->MaxBytes];
-            //this->BufferSize = 0;
-            this->NextBytes = 0;
-            this->NextField = 0;
-        }
+int FixedTextBuffer::Unpack(char *str){
+    int start = NextCharecter;
+    int packSize = FieldSizes[NextField];
+    strncpy(str, &Buffer[start], packSize);
+    NextCharecter += packSize;
+    str[packSize] = 0;
+    NextField ++;
+    return 0;
+}
 
-        int Unpack(char *str){
-            int start = NextCharecter;
-            int packSize = FieldSizes[NextField];
-            strncpy(str, &Buffer[start], packSize);
-            NextCharecter += packSize;
-            str[packSize] = 0;
-            NextField ++;
-            return 0;
-        }
+int FixedTextBuffer::Pack(char *str){
+    if (!Packing){
+        return false;
+    }
 
-        int Pack(char *str){
-            if (!Packing){
-                return false;
-            }
+    int length = strlen(str);
+    int start = NextCharecter;
+    int packSize = FieldSizes[NextField];
 
-            int length = strlen(str);
-            int start = NextCharecter;
-            int packSize = FieldSizes[NextField];
+    strncpy(&Buffer[start], str, packSize);
+    NextCharecter += packSize;
+    NextField ++;
 
-            strncpy(&Buffer[start], str, packSize);
-            NextCharecter += packSize;
-            NextField ++;
+    for(int i = (start+length); i < (start+ packSize); i++){
+        Buffer[i] = ' ';
+    }
 
-            for(int i = (start+length); i < (start+ packSize); i++){
-                Buffer[i] = ' ';
-            }
+    Buffer[NextCharecter] = 0;
 
-            Buffer[NextCharecter] = 0;
+    if (NextField == NumberOfFields){
+        Packing = false;
+        NextField = NextCharecter = 0;
+    }
 
-            if (NextField == NumberOfFields){
-                Packing = false;
-                NextField = NextCharecter = 0;
-            }
+    return true;
+}
 
-            return true;
-        }
+int FixedTextBuffer::Read(std::istream &input){
+    Clear();
+    input.read(Buffer, BufferSize);
+    return input.good();
+}
 
-        int Read(std::istream &input){
-            Clear();
-            input.read(Buffer, BufferSize);
-            return input.good();
-        }
-
-        int Write(std::ostream &output){
-            output.write(Buffer, BufferSize);
-            return output.good();
-        }
-};
+int FixedTextBuffer::Write(std::ostream &output){
+    output.write(Buffer, BufferSize);
+    return output.good();
+}
 
 int main(){
     std::ofstream output;
---
+++ FixedTextBuffer.h
@@ -0,0 +1,27 @@
+#ifndef FIXEDTEXTBUFFER_H
+#define FIXEDTEXTBUFFER_H
+#include<iostream>
+
+class FixedTextBuffer{
+    char *Buffer;
+    int BufferSize;
+    int MaxBytes;
+    int NextBytes;
+    int *FieldSizes;
+    int MaxFields;
+    int NumberOfFields;
+    int Packing;
+    int NextField;
+    int NextCharecter;
+
+    public:
+        FixedTextBuffer(int MaxBytes=60, int MaxFields=6);
+        int AddField(int FieldSize);
+        int GetNumberOfFields() const;
+        void Clear();
+        int Unpack(char *str);
+        int Pack(char *str);
+        int Read(std::istream &input);
+        int Write(std::ostream &output);
+};
+#endif
